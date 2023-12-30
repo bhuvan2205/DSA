def38871dd24274c8ca4ac740c09345059921c67
bhuvan2205 <bhuvancs2205@gmail.com>
2023-12-30 20:01:21 +0530

    Feat: Insertion sort - Optimized

--- sorting-algorithms/insertionSort.js
+++ sorting-algorithms/insertionSort.js
@@ -14,3 +14,23 @@ const insertionSort = (arr) => {
 };
 
 console.log(insertionSort([2, 1, 0])); // [0, 1, 2]
+
+/** 
+ 
+OPTIMIZED VERSION - INSERTION SORT
+
+const insertionSort = (arr) => {
+  if (arr.length === 0) return [];
+
+  for (let i = 1; i < arr.length; i++) {
+    const pointer = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > pointer; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = pointer;
+  }
+
+  return arr;
+};
+
+*/
